73f3f4385979e892c79911eedd07e6cb5449a4e5
Jose F Nieves <nieves@1-loop.net>
2012-04-28 10:40:13 -0400

    Updated debian mk.sh and mknpstats.sh

--- build/debian/mk.sh
+++ build/debian/mk.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
-. ../../VERSION
+branchname=nbsp
 
-cd ..
-cp -R debian ..
+cd ../../..
+. ./${branchname}/VERSION
 
-cd ..
-dpkg-buildpackage -rfakeroot
-cp ../${name}_${version}*.deb debian
+rm -rf ${name}-${version}
+cp -r $branchname ${name}-${version}
+
+cd ${name}-${version}
+cp -R build/debian .
+dpkg-buildpackage -rfakeroot -uc -us
+cp ../${name}_${version}*.deb build/debian
+rm -rf debian
 
 cd build/debian
 ./ckplist.sh ${name}_${version}*.deb
