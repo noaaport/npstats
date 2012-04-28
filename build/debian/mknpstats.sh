06859eedcc7b1dff4441b720ca35649f55f2ce5f
Jose F Nieves <nieves@1-loop.net>
2012-04-28 10:35:57 -0400

    Updated

--- build/debian/mknpstats.sh
+++ build/debian/mknpstats.sh
@@ -1,17 +1,6 @@
 #!/bin/sh
 
 name=npstats
-tgzfile=${name}.tgz
 
-rm -rf $name
-tar -xzf $tgzfile
-
-. $name/VERSION
-
-cd $name
-cp -R build/debian .
-dpkg-buildpackage -rfakeroot
-cp ../${name}_${version}*.deb build/debian
-
-cd build/debian
-./ckplist.sh ${name}_${version}*.deb
+cd $name/build/debian
+./mk.sh
